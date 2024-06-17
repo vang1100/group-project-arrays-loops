ef377278849562849946836b6d06fc071fbbbbda
Padee Vang <vangpadee11@gmail.com>
2024-06-16 21:01:38 -0500

    unfinished

--- scripts/client.js
+++ scripts/client.js
@@ -1,1 +1,9 @@
 console.log('JS sourced');
+
+let hats = ['stocking cap', 'baseball cap', 'fedora', 'top hat'];
+
+let hatList = document.querySelector('#new-inner-dom');
+
+for (let hat of hats) {
+  hatList.innerHTML += `<li>${hat}</li>`;
+}
